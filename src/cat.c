062bf60f2cda6fa38e50bffb1b18f15ce2142bbd
Ryan Cohen <rcohenprogramming@gmail.com>
2022-08-26 13:36:51 -0400

    cat: Concatenate multiple input streams instead of a single one.

--- src/cat.c
+++ src/cat.c
@@ -30,9 +30,37 @@ void usage() {
       "  -h, --help    display this help message and exit");
 }
 
-int main(int argc, char **argv) {
+/* contatenate input stream to stdout */
+int concat_stream(FILE *stream) {
   char buffer[BUFSIZ];
-  FILE *file = NULL;
+
+  /* copy from input stream to output stream */
+  while (true) {
+    /* read and check for error */
+    size_t bytes_read = fread((void *)&buffer, sizeof(char), BUFSIZ, stream);
+    if (bytes_read != BUFSIZ) {
+      if (ferror(stream)) {
+        return 1;
+      }
+    }
+
+    /* write and check for error */
+    size_t bytes_written = fwrite((const void *)&buffer, sizeof(char),
+        bytes_read, stdout);
+    if (bytes_written != bytes_read) {
+      return 1;
+    }
+
+    /* break if at end of stream */
+    if (feof(stream) || bytes_read == 0) {
+      break;
+    }
+  }
+  
+  return 0;
+}
+
+int main(int argc, char **argv) {
   int status = 0;
 
   if (argc > 1) {
@@ -42,38 +70,34 @@ int main(int argc, char **argv) {
     }
   }
 
-  if (argc < 2 || !strcmp(argv[1], "-")) {
-    /* use stdin as input */
-    file = stdin;
-  } else {
-    /* read file as input */
-    const char *filename = argv[1];
-    file = fopen(filename, "r");
-  }
-  ENSURE(file);
-
-  /* copy from input stream to output stream */
-  while (true) {
-    /* read and check for error */
-    size_t bytes_read = fread((void *)&buffer, sizeof(char), BUFSIZ, file);
-    if (bytes_read != BUFSIZ) {
-      ENSURE(!ferror(file));
-    }
-
-    /* write and check for error */
-    size_t bytes_written = fwrite((const void *)&buffer, sizeof(char),
-        bytes_read, stdout);
-    ENSURE(bytes_written == bytes_read);
-
-    /* break if at end of file */
-    if (feof(file) || bytes_read == 0) {
-      break;
-    }
-  }
-
-  if (file != stdin) {
-    status = fclose(file);
+  /* use stdin as input stream if there are no arguments */
+  if (argc < 2) {
+    status = concat_stream(stdin);
     ENSURE(!status);
+    return 0;
+  }
+
+  /* concatenate each input stream */
+  for (int i = 1; i < argc; ++i) {
+    FILE *stream = NULL;
+
+    if (!strcmp(argv[i], "-")) {
+      /* use stdin as input */
+      stream = stdin;
+    } else {
+      /* read file as input */
+      const char *filename = argv[i];
+      stream = fopen(filename, "r");
+    }
+    ENSURE(stream);
+    
+    status = concat_stream(stream);
+    ENSURE(!status);
+
+    if (stream != stdin) {
+      status = fclose(stream);
+      ENSURE(!status);
+    }
   }
 
   return 0;
